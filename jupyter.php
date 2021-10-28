71d4ec7bd8b65b84ade2820391451aa7b5481364
jiangyx <jiangyx@mingyuanyun.com>
2021-10-28 18:02:01 +0800

    提交插件

---
+++ jupyter.php
@@ -0,0 +1,26 @@
+<?php
+
+function jupyter_enqueue_style() {
+	wp_enqueue_style( 'Jupyter', plugins_url( '/css/jupyter.css', __FILE__ ));
+}
+
+function jupyter_handler($atts) {
+
+    //process plugin
+    extract(shortcode_atts(array(
+        'url' => "",
+    ), $atts));
+
+    $frame = '<iframe 
+        class="jupyter_frame"
+        frameborder="no"  
+        scrolling="no" 
+    src="'.$url.'" ></iframe>';
+
+    //send back text to calling function
+    return $frame;
+
+}
+
+add_action( 'wp_enqueue_scripts', 'jupyter_enqueue_style' );
+add_shortcode("jupyter", "jupyter_handler");
